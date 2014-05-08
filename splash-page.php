c87fb099516925bf6757fd0bfe9eb311fec00e16
laras126 <laras126@gmail.com>
2014-05-08 16:59:08 -0400

    make get template part work

--- splash-page.php
+++ splash-page.php
@@ -6,13 +6,10 @@ Template Name: Splash Page
 
 <div class="container">
 	
-	<?php include( 'templates/suya/splash-head.php'); ?>
-	
-	<?php 
-	// get_template_part not working here for some reason
-		include( 'templates/suya/takeaways.php'); ?>
-	<?php include( 'templates/suya/what-you-learn.php'); ?>
-	<?php include( 'templates/suya/instructors.php'); ?>
-	<?php include( 'templates/suya/testimonials.php'); ?>
+	<?php get_template_part('templates/suya/content', 'splashhead'); ?>
+	<?php get_template_part('templates/suya/content', 'takeaways'); ?>
+	<?php get_template_part('templates/suya/content', 'whatyoulearn'); ?>
+	<?php get_template_part('templates/suya/content', 'instructors'); ?>
+	<?php get_template_part('templates/suya/content', 'testimonials'); ?>
 
 </div>
