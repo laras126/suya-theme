f709421d717538648b1ddb5a658a1b0fa5baa384
laras126 <laras126@gmail.com>
2014-05-19 16:23:38 -0400

    fix acf maybe

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0752fe191420f8f7fd2a43147f966c51');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8771fff7f95f3bcde6cded3b888aa9b0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/suya/content-takeaways.php
+++ templates/suya/content-takeaways.php
@@ -7,31 +7,31 @@ $recent = new WP_Query("pagename=takeaways");
 while($recent->have_posts()) : $recent->the_post();?>
 
 <section class="sy--takeaways">
-	<h1 class="sy-title"><?php the_field('post_title'); ?></h1>
+	<h1 class="sy-title"><?php the_title(); ?></h1>
 	<p class="sy-tag text-center">
 		<?php the_field('takeaway_tagline'); ?>
 	</p>
-	<div class="row">
-		<div class="col-sm-4 sy-section sy-third">
+	<div class="row text-center">
+		<div class="sy-section sy-third">
 			<div class="text-center sy-icon">
 				<i class="fa <?php the_field('takeaway_left_icon'); ?>"></i>
 			</div>
 			<h4><?php the_field('takeaway_left_title'); ?></h4>
-			<p><?php the_field('takeaway_left_blurb'); ?></p>
+			<p class="small"><?php the_field('takeaway_left_blurb'); ?></p>
 		</div>
-		<div class="col-sm-4 sy-section sy-third">
+		<div class="sy-section sy-third">
 			<div class="text-center sy-icon">
 				<i class="fa <?php the_field('takeaway_center_icon'); ?>"></i>
 			</div>
 			<h4><?php the_field('takeaway_center_title'); ?></h4>
-			<p><?php the_field('takeaway_center_blurb'); ?></p>
+			<p class="small"><?php the_field('takeaway_center_blurb'); ?></p>
 		</div>
-		<div class="col-sm-4 sy-section sy-third">
+		<div class="sy-section sy-third">
 			<div class="text-center sy-icon">
 				<div class="fa <?php the_field('takeaway_right_icon'); ?>"></div>
 			</div>
 			<h4><?php the_field('takeaway_right_title'); ?></h4>
-			<p><?php the_field('takeaway_right_blurb'); ?></p>
+			<p class="small"><?php the_field('takeaway_right_blurb'); ?></p>
 		</div>
 	</div>
 	<?php edit_post_link(); ?>
