7a0449ad93310e3944e923b52fcbde5645c9fc35
laras126 <laras126@gmail.com>
2014-05-26 12:05:25 -0400

    center instructors text

--- templates/suya/content-instructors.php
+++ templates/suya/content-instructors.php
@@ -3,12 +3,12 @@
 
 $recent = new WP_Query("pagename=instructors"); 
 while($recent->have_posts()) : $recent->the_post();?>
-	<section class="sy--instructors">
+	<section>
 		<h1 class="sy-title"><?php the_title(); ?></h1>
 		<p class="sy-tag text-center">
 			<?php the_field('instructor_tagline'); ?>
 		</p>
-		<div class="row">
+		<div class="row text-center">
 			<div class="col-sm-6 sy-half sy-section">
 				<img class="img-responsive center-block" src="<?php the_field('instructor_left_image'); ?>" alt="<?php the_field('instructor_left_name'); ?>">
 				<h2><?php the_field('instructor_left_name'); ?></h2>
